dc95e27618e5b6fb9e5f744594e5fba78ac2a782
Aidahar <zagirik@gmail.com>
2019-04-09 17:01:12 +0300

    add style and form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,5 +14,12 @@ class UsersController < ApplicationController
       name: "Mr.Gru",
       username: "Gru"
     )
+
+    @questions = [
+      Question.new(text: "How do you do?", created_at: Date.parse('27.03.2019')),
+      Question.new(text: "Are you pussy?", created_at: Date.parse('27.03.2019'))
+    ]
+    
+    @new_question = Question.new
   end
 end

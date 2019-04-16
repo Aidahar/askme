d8db24d862412d3a39e9fedde8dfc72b2ae3c284
Aidahar <zagirik@gmail.com>
2019-04-16 12:43:13 +0300

    add last migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190412154901) do
+ActiveRecord::Schema.define(version: 20190415151905) do
 
   create_table "questions", force: :cascade do |t|
     t.string   "text"
@@ -19,9 +19,8 @@ ActiveRecord::Schema.define(version: 20190412154901) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.integer  "author_id"
   end
-
-  add_index "questions", ["user_id"], name: "index_questions_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -35,6 +34,7 @@ ActiveRecord::Schema.define(version: 20190412154901) do
     t.string   "bg_color"
   end
 
+  add_index "questions", ["author_id"], name: "index_questions_on_author_id"
+  add_index "questions", ["user_id"], name: "index_questions_on_user_id"
   add_index "users", ["username"], name: "index_users_on_username", unique: true
-
 end

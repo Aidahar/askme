5b2cef3da1b857a60e34ea47e9f43a537c0216f8
Aidahar <zagirik@gmail.com>
2019-05-22 22:16:30 +0300

    fix recaptcha

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,4 @@ Rails.application.routes.draw do
   resources :users, except: [:destroy]
   resource :session, only: [:new, :create, :destroy]
   resources :questions, except: [:show, :new, :index]
-
 end

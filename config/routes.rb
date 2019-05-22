a2c2ae413a3154243904fc6eab658ea324b42db7
Aidahar <zagirik@gmail.com>
2019-05-22 14:10:14 +0300

    fix misprint

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'users#index'
 
   resources :users, except: [:destroy]
-  resource :sessions, only: [:new, :create, :destroy]
+  resource :session, only: [:new, :create, :destroy]
   resources :questions, except: [:show, :new, :index]
+
 end
